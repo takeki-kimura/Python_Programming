d1c4de8bb47e8dbd74e868550d8a42983521f179
TakekiKimura <takeki.kimura.0209@gmail.com>
2021-08-20 16:54:21 +0900

    文字列

--- main.py
+++ main.py
@@ -14,3 +14,61 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+
+>>> 2 +2
+4
+>>> 5 - 2
+3
+>>> 50 - 5 * 6
+20
+>>> 5 *6
+30
+>>> (50 - 5) * 6
+270
+>>> 8 / 5
+1.6
+
+>>> 'a is {}' .format('a')
+'a is a'
+
+>>> 'a is {}' .format('test')
+'a is test'
+
+>>> 'a is {} {} {}'.format(1,2,3)
+'a is 1 2 3'
+
+>>> 'a is {0} {1} {2}'.format(1,2,3)
+'a is 1 2 3'
+
+>>> 'a is {2} {1} {0}'.format(1,2,3)
+'a is 3 2 1'
+
+>>> 'My name is {0} {1}'.format('Takeki','Kimura')
+'My name is Takeki Kimura'
+
+>>> 'My name is {0} {1}.watashi ha {1} {0}'.format('Takeki','Kimura')
+'My name is Takeki Kimura.watashi ha Kimura Takeki'
+
+>>> 'My name is {name} {family}.watashi ha {family} {name}'.format(name = 'Takeki',family = 'Kimura')
+'My name is Takeki Kimura.watashi ha Kimura Takeki'
+
+>>> 1
+1
+>>> '1'
+'1'
+>>> str(1)
+'1'
+
+>>> x = str(1)
+>>> type(x)
+<class 'str'>
+
+>>> str(3.14)
+'3.14'
+
+>>> str(True)
+'True'
+
+"""
