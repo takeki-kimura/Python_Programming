9329b60cc3571cfc43ba767243971a016724e087
TakekiKimura <takeki.kimura.0209@gmail.com>
2021-08-24 10:54:54 +0900

    リストのメソッド

---
+++ lesson3.py
@@ -0,0 +1,29 @@
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3,3))
+
+print(r.count(3))
+
+if 5 in r:
+    print('exist')
+
+if 100 in r:
+    print('exist')
+
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+
+s = 'My name is Mike'
+to_split = s.split(' ')
+print(to_split)
+
+x = ' '.join(to_split)
+print(x)
+
+x = ' ###### '.join(to_split)
+print(x)
+
+print(help(list))
