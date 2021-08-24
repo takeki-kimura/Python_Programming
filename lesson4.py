daea39d09ae75d91875c239ea2ede8af0c8ea5b0
TakekiKimura <takeki.kimura.0209@gmail.com>
2021-08-24 11:25:53 +0900

    リストのコピー

---
+++ lesson4.py
@@ -0,0 +1,28 @@
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j =', j)
+print('i =', i)
+
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+# y = x[:]
+y[0] = 100
+print('y =', y)
+print('x =', x)
+
+X = 20
+Y = x
+Y = 5
+print(id(X))
+print(id(Y))
+print(Y)
+print(X)
+
+X = ['a', 'b']
+Y = X
+Y[0] = 'p'
+print(id(X))
+print(id(Y))
+print(Y)
+print(X)
